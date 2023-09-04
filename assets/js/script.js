64a1172921bf035a05c67dfb47ba74cf0d2671be
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-04 12:05:17 +0200

    add selector difficulty

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,7 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-
 const playButtonEl = document.getElementById("play-button");
 const resetButtonEl = document.getElementById("reset-button");
 const gridEl = document.getElementById("grid");
-const titleEl = document.getElementById("title");
-const numberCells = 100;
+const menuEl = document.getElementById("menu");
 
 /**
  * Generate a grid with n cells
@@ -17,6 +11,7 @@ function generateCells(numberCells) {
    for (let i = 0; i < numberCells; i++) {
       const cell = document.createElement("div");
       cell.classList.add("cell");
+      cell.style.width = `calc(100% / ${Math.sqrt(numberCells)})`;
       cell.innerHTML = i + 1;
       gridEl.append(cell);
 
@@ -33,24 +28,30 @@ function generateCells(numberCells) {
  * @param {DomEl} domEl2 
  * @param {DomEl} domEl3 
  */
-function toggle (domEl1, domEl2, domEl3){
+function toggle (domEl1){
    domEl1.classList.toggle("d-none");
-   domEl2.classList.toggle("d-none");
-   domEl3.classList.toggle("d-none");
 }
 
 playButtonEl.addEventListener("click", function(){
+   const numberCells = document.querySelector("select").value;
    generateCells(numberCells);
-
-   toggle(playButtonEl, resetButtonEl, titleEl);
+   toggle(menuEl);
+   toggle(resetButtonEl);
+   console.log(numberCells);
 
 })
 
 resetButtonEl.addEventListener("click", function(){
    gridEl.innerHTML = "";
 
-   toggle(playButtonEl, resetButtonEl, titleEl);
+   toggle(menuEl);
+   toggle(resetButtonEl);   
 })
+
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
 
